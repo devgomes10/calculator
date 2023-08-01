e79dc0ed0c13bf25cd8a2f07491ffb171e384c1e
Vinicius Gomes Correia <viniciusgomesccc10@gmail.com>
2023-08-01 20:31:55 -0300

    implementado layout da calculadora

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ class AppCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Calculadora',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.pink),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
       home: Calculator(),
     );
   }
@@ -35,74 +38,124 @@ class _CalculatorState extends State<Calculator> {
 
   void _calculateResult() {
     try {
-
+      var number = int.parse(_buildButton);
     } catch (e) {
       setState(() {
         _displayText = 'Erro';
       });
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('Calculadora', style: TextStyle(fontSize: 30),),
+        title: Text(
+          'Calculadora',
+          style: TextStyle(fontSize: 30),
+        ),
         centerTitle: true,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
+          Padding(
+            padding: const EdgeInsets.only(bottom: 80, top: 30),
             child: Container(
+              width: 350,
+              height: 130,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(40),
+              ),
               padding: EdgeInsets.all(16),
               alignment: Alignment.bottomRight,
-              child: Text(
-                _displayText,
-                style: TextStyle(fontSize: 32),
+              child: Center(
+                child: Text(
+                  _displayText,
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ),
-          Divider(height: 1, color: Colors.grey),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildButton('7'),
-              _buildButton('8'),
-              _buildButton('9'),
-              _buildButton('+'),
-            ],
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Colors.pink,
+                    width: 7,
+                  )),
+              width: 350,
+              height: 320,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildButton('7', textColor: Colors.white),
+                          _buildButton('8', textColor: Colors.white),
+                          _buildButton('9', textColor: Colors.white),
+                          _buildButton('+', textColor: Colors.white),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildButton('4', textColor: Colors.white),
+                          _buildButton('5', textColor: Colors.white),
+                          _buildButton('6', textColor: Colors.white),
+                          _buildButton('-', textColor: Colors.white),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildButton('1', textColor: Colors.white),
+                          _buildButton('2', textColor: Colors.white),
+                          _buildButton('3', textColor: Colors.white),
+                          _buildButton('*', textColor: Colors.white),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildButton('0', textColor: Colors.white),
+                          _buildButton('.', textColor: Colors.white),
+                          _buildButton('C', textColor: Colors.white),
+                          _buildButton('/', textColor: Colors.white),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildButton('4'),
-              _buildButton('5'),
-              _buildButton('6'),
-              _buildButton('-'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildButton('1'),
-              _buildButton('2'),
-              _buildButton('3'),
-              _buildButton('*'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildButton('0'),
-              _buildButton('.'),
-              _buildButton('C', textColor: Colors.red),
-              _buildButton('/', textColor: Colors.green),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: _calculateResult,
-            child: Text('Calcular', style: TextStyle(fontSize: 20)),
+          Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 10),
+            child: ElevatedButton(
+              onPressed: _calculateResult,
+              child: Text('Calcular', style: TextStyle(fontSize: 20)),
+            ),
           ),
         ],
       ),
@@ -118,9 +171,9 @@ class _CalculatorState extends State<Calculator> {
           _addToDisplay(text);
         }
       },
-      child: Text(text, style: TextStyle(color: textColor, fontSize: 20)),
+      child: Text(text, style: TextStyle(color: textColor, fontSize: 30)),
       style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.all(16),
+        padding: EdgeInsets.all(10),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
       ),
     );
